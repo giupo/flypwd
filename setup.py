34c22a9e58de787e70498ec18185a70303063c57
Giuseppe Acito <giuseppe.acito@bancaditalia.it>
2017-01-24 14:32:57 +0000

    fix file perm mess and bumpversion

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ test_requirements = [
 
 setup(
     name='flypwd',
-    version='0.2.3',
+    version='0.2.4',
     description='Library to store and retrieve passwords',
     long_description=readme + '\n\n' + history,
     author='Giuseppe Acito',
