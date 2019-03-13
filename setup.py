7bdfafd9fca20536134924a1b28f5a2bf1083973
Giuseppe Acito <giuseppe.acito@gmail.com>
2019-03-13 18:46:18 +0100

    works on 3.7

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setup(
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.7',
     ],
     cmdclass={'test': PyTest},
     test_suite='tests',
